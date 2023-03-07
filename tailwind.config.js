42c0f9aa862327d295395a5fe642f4b0b408af76
sahujyoti50 <sahujyoti50@gmail.com>
2023-03-07 11:07:37 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  backgroundImage: {
+    'flag': "url('./public/img/Flag_of_Hungary.svg')",
+  }
+}
